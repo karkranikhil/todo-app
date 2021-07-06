444995ec6bbc3a995950521e1c58ba32505f0b53
Nikhil Karkra <karkra.nikhil@gmail.com>
2021-07-06 21:13:13 +1000

    backend

---
+++ server/index.js
@@ -0,0 +1,32 @@
+// require express
+const  express = require("express");
+
+//load env variables
+require('dotenv').config()
+console.log(process.env.REACT_APP_DB_USER)
+// create express app
+const  app = express();
+
+// Import DB Connection
+require("./config.js");
+
+// Import API route
+var routes = require('./routes'); //importing route
+routes(app);
+
+// define port to run express app
+const  port = process.env.PORT || 5000;
+
+// use middleware on express app
+app.use(express.json());
+
+// Add endpoint
+app.get('/', (req, res) => {
+    res.send("Welcome to our Todo App");
+    });
+    
+// Listen to server
+app.listen(port, () => {
+
+console.log(`Server running at http://localhost:${port}`);
+});

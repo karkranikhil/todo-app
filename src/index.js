489904b66d27437076bb63b207a51c7c64a9f163
BSH-GGS <bsh-ggs@BSH-GGSs-MacBook-Pro.local>
2021-07-06 12:00:49 +0530

    added redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore } from "./store";
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(
+    <Provider store={configureStore()}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'),
+);

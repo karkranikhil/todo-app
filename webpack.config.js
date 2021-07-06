3ca2f28105e2913ccc795aeecd56c5f59753d801
BSH-GGS <bsh-ggs@BSH-GGSs-MacBook-Pro.local>
2021-07-06 14:33:48 +0530

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -24,14 +24,14 @@ module.exports = {
         extensions: ['*', '.js', '.jsx']
     },
     output: {
-        path: path.resolve(__dirname, 'dist/'),
-        publicPath: '/dist/',
+        path: path.resolve(__dirname, 'public/'),
+        publicPath: '/public/',
         filename: 'bundle.js'
     },
     devServer: {
         contentBase: path.join(__dirname, 'public/'),
         port: 3000,
-        publicPath: 'http://localhost:3000/dist/',
+        publicPath: 'http://localhost:3000/public/',
         hotOnly: true
     },
     plugins: [new webpack.HotModuleReplacementPlugin()]
